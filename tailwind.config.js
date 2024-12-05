2f14bd744994b27c5aef6592e05a76656cd1bb95
Allen-pogi <allenpaulrebolla@gmail.com>
2024-12-05 10:58:25 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx,tsx,ts}"],
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
