c88f58fef1e18d63c08230d813e5da97a6595eef
Allen-pogi <allenpaulrebolla@gmail.com>
2024-12-05 11:05:03 +0800

    update

--- src/skills/skills.jsx
+++ src/skills/skills.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Skills = () => {
     return(
 <div>
-<h3 className="text-white text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4">Skills</h3>
-            <div className="flex gap-3 p-3 flex-wrap pr-4">
+<h3 className="text-white text-lg font-bold leading-tight tracking-[-0.015em] px-4 pb-2 pt-4 mt-8">Skills</h3>
+            <div className="flex gap-3  p-3 flex-wrap pr-4">
               <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
                 <p className="text-white text-sm font-medium leading-normal">JavaScript</p>
               </div>
