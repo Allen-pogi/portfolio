e60894ce0ef390345a91e7421c26ea1008c1b2ff
Allen-pogi <allenpaulrebolla@gmail.com>
2024-12-13 22:23:02 +0800

    update

--- src/app.route.jsx
+++ src/app.route.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./homepage";
-import ParticlesBackground from "./background/background";
 
 function App() {
   return (
